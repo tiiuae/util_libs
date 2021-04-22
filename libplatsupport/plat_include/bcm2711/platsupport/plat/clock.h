2f3a6aa462fa974536f2cb1dd41d3c2281e585a5
Nick Spinale <nick@nickspinale.com>
2021-04-22 16:48:09 +0300

    Cherry-picked commits from IceCap repository.
    Added/merged fixes from IceCap repo to libplatsupport.
    Also updated header license information where necessary.
    
    IceCap repository URL: https://gitlab.com/arm-research/security/icecap/minor-patches/sel4/util_libs.git
    Branch: icecap
    
    -5955d26d: "rpi4: fix printf warnings"
    -c5dc69e0: "rpi4: fix mini uart driver"
    -f743e24e: "rpi4: copy bcm2837 to bcm2711"

--- libplatsupport/plat_include/bcm2711/platsupport/plat/clock.h
+++ libplatsupport/plat_include/bcm2711/platsupport/plat/clock.h
@@ -1,9 +1,10 @@
 /*
- * Copyright 2017, Data61, CSIRO (ABN 41 687 119 230)
+ * Copyright 2017, Data61, CSIRO (ABN 41 687 119 230).
  * Copyright (C) 2021, Hensoldt Cyber GmbH
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
+
 #pragma once
 
 enum clk_id {
