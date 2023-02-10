b3e8c2bc1fdb0f029b7d157bf6a66dc37ed207cf
Joonas Onatsu <joonas.onatsu@unikie.com>
2023-02-10 16:55:49 +0200

    WIP: add mbedtls v3.3.0
    mbedtls is currently added as "hard" dependency,
    meaning the whole source repo is copied here.
    This won't fly in the long run, but before
    CMake's ExternalProject_Add can be brute-forced
    enough to add this as an external project, the
    copy stays.
    
    Also there is no reasonable config for the library
    currently, so it doesn't probably work right.
    
    Signed-off-by: Joonas Onatsu <joonasx@ssrc.tii.ae>

---
+++ 3rdparty/mbedtls/settings.cmake
@@ -0,0 +1,25 @@
+#
+# Copyright 2023, Technology Innovation Institute
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+# Use the same version as mbedtls wants.
+cmake_minimum_required(VERSION 3.5.1)
+
+set(ENABLE_PROGRAMS            OFF CACHE BOOL "Don't build mbedtls programs" FORCE)
+set(UNSAFE_BUILD               OFF CACHE BOOL "Don't allow unsafe builds for mbedtls" FORCE)
+set(MBEDTLS_FATAL_WARNINGS     OFF CACHE BOOL "Don't treat compiler warnings treated as errors on mbedtls" FORCE)
+set(ENABLE_TESTING             OFF CACHE BOOL "Don't build mbedtls tests" FORCE)
+set(USE_STATIC_MBEDTLS_LIBRARY ON CACHE BOOL  "Build mbed TLS static library" FORCE)
+set(USE_SHARED_MBEDTLS_LIBRARY OFF CACHE BOOL "Don't build mbed TLS shared library" FORCE)
+set(LINK_WITH_PTHREAD          OFF CACHE BOOL "Don't link mbed TLS library to pthread" FORCE)
+set(LINK_WITH_TRUSTED_STORAGE  OFF CACHE BOOL "Don't link mbed TLS library to trusted_storage" FORCE)
+
+# Ugly hack to add muslc dependency. This is to keep
+# changes to the mbedtls sources as small as possible.
+# This will most likely break in unexpected ways many times :)
+# Used by ./library/CMakeLists.txt
+set(libs ${libs} muslc)
+
+# WIP: add custom config for mbedtls, default likely doesn't work.
